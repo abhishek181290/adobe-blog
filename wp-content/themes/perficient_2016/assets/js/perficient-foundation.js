fc2ab064c392ab756e5ad5640bf4db2701fca1f7
Dan Klco <dan.klco@sixdimensions.com>
2016-06-23 15:14:11 -0400

    Initial commit

---
+++ wp-content/themes/perficient_2016/assets/js/perficient-foundation.js
@@ -0,0 +1,9 @@
+﻿function isPageEditor() {
+    if (typeof Sitecore == "undefined") {
+        return false;
+    }
+    if (typeof Sitecore.PageModes == "undefined" || Sitecore.PageModes == null) {
+        return false;
+    }
+    return Sitecore.PageModes.PageEditor != null;
+}
